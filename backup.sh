56d4317c1f8e24834e2cbd38f6064bbfae94e59a
santanaoliva_u <urielsantanaoliva@gmail.com>
2025-01-05 21:10:37 -0500

    add all

--- backup.sh
+++ backup.sh
@@ -2,67 +2,66 @@
 
 # This checks if the number of arguments is correct
 # If the number of arguments is incorrect ( $# != 2) print error message and exit
-if [[ $# != 2 ]]
-then
+if [[ $# != 2 ]]; then
   echo "backup.sh target_directory_name destination_directory_name"
   exit
 fi
 
 # This checks if argument 1 and argument 2 are valid directory paths
-if [[ ! -d $1 ]] || [[ ! -d $2 ]]
-then
+if [[ ! -d $1 ]] || [[ ! -d $2 ]]; then
   echo "Invalid directory path provided"
   exit
 fi
 
 # [TASK 1]
-targetDirectory=
-destinationDirectory=
+targetDirectory=$1
+destinationDirectory=$2
 
 # [TASK 2]
-echo ""
-echo ""
+echo "Target Directory: $targetDirectory"
+echo "Destination Directory: $destinationDirectory"
 
 # [TASK 3]
-currentTS=``
+currentTS=$(date +%s)
 
 # [TASK 4]
-backupFileName=""
+backupFileName="backup-[$currentTS].tar.gz"
 
 # We're going to:
-  # 1: Go into the target directory
-  # 2: Create the backup file
-  # 3: Move the backup file to the destination directory
+# 1: Go into the target directory
+# 2: Create the backup file
+# 3: Move the backup file to the destination directory
 
 # To make things easier, we will define some useful variables...
 
 # [TASK 5]
-origAbsPath=``
+origAbsPath=$(pwd)
 
 # [TASK 6]
-cd # <-
-destDirAbsPath=``
+cd "$destinationDirectory"
+destAbsPath=$(pwd)
 
 # [TASK 7]
-cd # <-
-cd # <-
+cd "$origAbsPath"
+cd "$targetDirectory"
 
 # [TASK 8]
-yesterdayTS=
+yesterdayTS=$(($currentTS - 24 * 60 * 60))
 
 declare -a toBackup
 
-for file in $() # [TASK 9]
-do
+for file in $( # [TASK 9]
+  ls
+); do
   # [TASK 10]
-  if (())
-  then
+  if (($(date -r $file +%s) > $yesterdayTS)); then
+    toBackup+=($file)
     # [TASK 11]
   fi
 done
 
 # [TASK 12]
+tar -czvf "$backupFileName" "${toBackup[@]}"
 
 # [TASK 13]
-
-# Congratulations! You completed the final project for this course!
+mv "$backupFileName" "$destAbsPath"
